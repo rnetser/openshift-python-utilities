fbf5090af30d8b95112e225920ea902e7e79ae72
Ruth Netser <54615090+rnetser@users.noreply.github.com>
2022-07-07 15:17:03 +0300

    Add kubeconfig option for must_gather (#14)

--- ocp_utilities/must_gather.py
+++ ocp_utilities/must_gather.py
@@ -9,8 +9,16 @@ from ocp_utilities.infra import run_command
 LOGGER = logging.getLogger(__name__)
 
 
-def create_must_gather_command(dest_dir, image_url, script_name=None):
-    base_command = f"oc adm must-gather --image={image_url} --dest-dir={dest_dir}"
+def create_must_gather_command(
+    dest_dir,
+    image_url,
+    script_name=None,
+    kubeconfig=None,
+):
+    base_command = (
+        f"oc adm must-gather {f'--kubeconfig {kubeconfig}' if kubeconfig else ''} --image={image_url}"
+        f" --dest-dir={dest_dir}"
+    )
     return f"{base_command} -- {script_name}" if script_name else base_command
 
 
@@ -19,7 +27,7 @@ def run_cnv_must_gather(must_gather_cmd):
     return run_command(command=shlex.split(must_gather_cmd))[1]
 
 
-def save_must_gather_logs(target_base_dir, must_gather_image_url):
+def save_must_gather_logs(target_base_dir, must_gather_image_url, kubeconfig=None):
     logs_path = os.path.join(
         target_base_dir,
         f"must_gather_{datetime.datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S')}",
@@ -28,5 +36,6 @@ def save_must_gather_logs(target_base_dir, must_gather_image_url):
     must_gather_command = create_must_gather_command(
         image_url=must_gather_image_url,
         dest_dir=logs_path,
+        kubeconfig=kubeconfig,
     )
     return run_cnv_must_gather(must_gather_cmd=must_gather_command)
