ad47ba1c30588a0f82eba7002304dcdec7c1614d
atandale <atandale@redhat.com>
2022-07-20 12:43:58 +0530

    added new exceptions

--- ocp_utilities/exceptions.py
+++ ocp_utilities/exceptions.py
@@ -4,3 +4,19 @@ class ClusterSanityError(Exception):
 
     def __str__(self):
         return self.err_str
+
+
+class NodeNotReadyError(Exception):
+    def __init__(self, err_str):
+        self.err_str = err_str
+
+    def __str__(self):
+        return self.err_str
+
+
+class NodeUnschedulableError(Exception):
+    def __init__(self, err_str):
+        self.err_str = err_str
+
+    def __str__(self):
+        return self.err_str
