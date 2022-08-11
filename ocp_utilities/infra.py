ed056e9e0815827aabf27b0f7a6b40f5a9efc1c2
Ruth Netser <54615090+rnetser@users.noreply.github.com>
2022-08-11 16:22:33 +0300

    for review (#28)

--- ocp_utilities/infra.py
+++ ocp_utilities/infra.py
@@ -1,16 +1,11 @@
-import logging
-import subprocess
-
-import kubernetes as kubernetes
-from kubernetes.dynamic import DynamicClient
-
-from ocp_utilities.exceptions import ClusterSanityError
+from ocp_utilities.exceptions import NodeNotReadyError, NodeUnschedulableError
+from ocp_utilities.logger import get_logger
 
 
-LOGGER = logging.getLogger(__name__)
+LOGGER = get_logger(name=__name__)
 
 
-def validate_nodes_ready(nodes):
+def assert_nodes_ready(nodes):
     """
     Validates all nodes are in ready
 
@@ -18,17 +13,17 @@ def validate_nodes_ready(nodes):
          nodes(list): List of Node objects
 
     Raises:
-        AssertionError: Assert on node(s) in not ready state
+        NodeNotReadyError: Assert on node(s) in not ready state
     """
     LOGGER.info("Verify all nodes are ready.")
     not_ready_nodes = [node.name for node in nodes if not node.kubelet_ready]
     if not_ready_nodes:
-        raise ClusterSanityError(
-            err_str=f"Following nodes are not in ready state: {not_ready_nodes}",
+        raise NodeNotReadyError(
+            f"Following nodes are not in ready state: {not_ready_nodes}"
         )
 
 
-def validate_nodes_schedulable(nodes):
+def assert_nodes_schedulable(nodes):
     """
     Validates all nodes are in schedulable state
 
@@ -36,52 +31,13 @@ def validate_nodes_schedulable(nodes):
          nodes(list): List of Node objects
 
     Raises:
-        AssertionError: Asserts on node(s) not schedulable
+        NodeUnschedulableError: Asserts on node(s) not schedulable
     """
     LOGGER.info("Verify all nodes are schedulable.")
     unschedulable_nodes = [
         node.name for node in nodes if node.instance.spec.unschedulable
     ]
     if unschedulable_nodes:
-        raise ClusterSanityError(
-            err_str=f"Following nodes are in unschedulable state: {unschedulable_nodes}",
+        raise NodeUnschedulableError(
+            f"Following nodes are in unscheduled state: {unschedulable_nodes}"
         )
-
-
-def get_admin_client():
-    return DynamicClient(client=kubernetes.config.new_client_from_config())
-
-
-def run_command(command, verify_stderr=True, shell=False):
-    """
-    Run command locally.
-
-    Args:
-        command (list): Command to run
-        verify_stderr (bool, default True): Check command stderr
-        shell (bool, default False): run subprocess with shell toggle
-
-    Returns:
-        tuple: True, out if command succeeded, False, err otherwise.
-    """
-    sub_process = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=shell,
-    )
-    out, err = sub_process.communicate()
-    out_decoded = out.decode("utf-8")
-    err_decoded = err.decode("utf-8")
-
-    error_msg = f"Failed to run {command}. rc: {sub_process.returncode}, out: {out_decoded}, error: {err_decoded}"
-    if sub_process.returncode != 0:
-        LOGGER.error(error_msg)
-        return False, out_decoded, err_decoded
-
-    # From this point and onwards we are guaranteed that sub_process.returncode == 0
-    if err_decoded and verify_stderr:
-        LOGGER.error(error_msg)
-        return False, out_decoded, err_decoded
-
-    return True, out_decoded, err_decoded
--- ocp_utilities/must_gather.py
+++ ocp_utilities/must_gather.py
@@ -1,46 +1,75 @@
 import datetime
-import logging
 import os
 import shlex
 
-from ocp_utilities.infra import run_command
+from ocp_utilities.logger import get_logger
+from ocp_utilities.utils import run_command
 
 
-LOGGER = logging.getLogger(__name__)
+LOGGER = get_logger(name=__name__)
 
 
-def create_must_gather_command(
-    dest_dir,
+def must_gather_command(
+    dest_dir=None,
     image_url=None,
+    skip_tls_check=False,
+    kubeconfig=None,
     script_name=None,
+):
+    base_command = "oc adm must-gather"
+    if dest_dir:
+        base_command += f" --dest-dir={dest_dir}"
+    if image_url:
+        base_command += f" --image={image_url}"
+    if skip_tls_check:
+        base_command += " --insecure-skip-tls-verify"
+    if kubeconfig:
+        base_command += f" --kubeconfig {kubeconfig}"
+    # script_name must be the last argument
+    if script_name:
+        base_command += f" -- {script_name}"
+
+    LOGGER.info(f"must-gather command: {base_command}")
+    return base_command
+
+
+def run_must_gather(
+    image_url=None,
+    target_base_dir=None,
     kubeconfig=None,
     skip_tls_check=False,
+    script_name=None,
 ):
-    base_command = (
-        f"oc adm must-gather {f'--insecure-skip-tls-verify' if skip_tls_check else ''} "
-        f"{f'--kubeconfig {kubeconfig}' if kubeconfig else ''} {f'--image={image_url}' if image_url else ''} "
-        f"--dest-dir={dest_dir}"
-    )
-    return f"{base_command} -- {script_name}" if script_name else base_command
+    """
+    Run must gather command with an option to create target directory.
 
+    Args:
+        image_url (str, optional): must-gather plugin image to run.
+            If not specified, OpenShift's default must-gather image will be used.
+        target_base_dir (str, optional): path to base directory
+        kubeconfig (str, optional): path to kubeconfig
+        skip_tls_check (bool, default: False): if True, skip tls check
+        script_name (str, optional): must-gather script name or path
 
-def run_cnv_must_gather(must_gather_cmd):
-    LOGGER.info(f"Running: {must_gather_cmd}")
-    return run_command(command=shlex.split(must_gather_cmd))[1]
+    Returns:
+        str: command output
+    """
+    dest_dir = None
+    if target_base_dir:
+        dest_dir = os.path.join(
+            target_base_dir,
+            f"must_gather_{datetime.datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S')}",
+        )
+        os.makedirs(dest_dir)
 
-
-def save_must_gather_logs(
-    target_base_dir, must_gather_image_url, kubeconfig=None, skip_tls_check=False
-):
-    logs_path = os.path.join(
-        target_base_dir,
-        f"must_gather_{datetime.datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S')}",
-    )
-    os.makedirs(logs_path)
-    must_gather_command = create_must_gather_command(
-        image_url=must_gather_image_url,
-        dest_dir=logs_path,
-        kubeconfig=kubeconfig,
-        skip_tls_check=skip_tls_check,
-    )
-    return run_cnv_must_gather(must_gather_cmd=must_gather_command)
+    return run_command(
+        command=shlex.split(
+            must_gather_command(
+                image_url=image_url,
+                dest_dir=dest_dir,
+                kubeconfig=kubeconfig,
+                skip_tls_check=skip_tls_check,
+                script_name=script_name,
+            )
+        )
+    )[1]
