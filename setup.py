cb24b5b0c56587f6e1e4483127ffd63201ccc4ff
Meni Yakove <441263+myakove@users.noreply.github.com>
2023-01-16 11:07:53 +0200

    Add openshift-python-wrapper as required (#95)
    Co-authored-by: Ruth Netser <rnetser@redhat.com>

--- setup.py
+++ setup.py
@@ -10,6 +10,13 @@ setup(
     keywords=["Openshift"],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["openshift", "colorlog", "pytest", "pytest-testconfig", "PyYAML"],
-    python_requires=">=3.6",
+    install_requires=[
+        "openshift",
+        "colorlog",
+        "pytest",
+        "pytest-testconfig",
+        "PyYAML",
+        "openshift-python-wrapper==4.12.*",
+    ],
+    python_requires=">=3.8",
 )
